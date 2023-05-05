8b2732d9aa26b4d8f62e14acdf110b5a0b7ef513
Vivekroy007 <132645277+Vivekroy007@users.noreply.github.com>
2023-05-05 18:33:56 +0530

    Add files via upload

---
+++ learning22.java
@@ -0,0 +1,9 @@
+class learning22{
+    public static void main(String[] args) {
+        System.out.println("this my 2nd file");
+        for (int i = 0; i <= 15; i++) {
+            System.out.println("this is u r no: "+i);
+            
+        }
+    }
+}
